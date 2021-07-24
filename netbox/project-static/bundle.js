GoteamQ/netbox

commit dcfa5d103597ea4df642455044c675991eb0f7a8

Migrate from ParcelJS to esbuild for UI bundling

netbox/project-static/bundle.js
@@ -1,66 +1,84 @@
-/**
- * ParcelJS Bundle Configuration.
- *
- * @see https://parceljs.org/api.html
- */
-
-const Bundler = require('parcel-bundler');
+const esbuild = require('esbuild');
+const { sassPlugin } = require('esbuild-sass-plugin');
 
 // Bundler options common to all bundle jobs.
 const options = {
-  logLevel: 2,
-  cache: true,
-  watch: false,
+  outdir: './dist',
+  bundle: true,
   minify: true,
-  outDir: './dist',
-  publicUrl: '/static',
+  sourcemap: true,
+  logLevel: 'error',
+  publicPath: '/static',
 };
 
 // Get CLI arguments for optional overrides.
-const args = process.argv.slice(2);
-
-// Allow cache disabling.
-if (args.includes('--no-cache')) {
-  options.cache = false;
-}
-
-// Style (SCSS) bundle jobs. Generally, everything should be bundled into netbox.css from main.scss
-// unless there is a specific reason to do otherwise.
-const styles = [
-  ['styles/_external.scss', 'netbox-external.css'],
-  ['styles/_light.scss', 'netbox-light.css'],
-  ['styles/_dark.scss', 'netbox-dark.css'],
-  ['styles/_rack_elevations.scss', 'rack_elevation.css'],
-  ['styles/_cable_trace.scss', 'cable_trace.css'],
-];
-
-// Script (JavaScript) bundle jobs. Generally, everything should be bundled into netbox.js from
-// index.ts unless there is a specific reason to do otherwise.
-const scripts = [
-  ['src/index.ts', 'netbox.js'],
-  ['src/jobs.ts', 'jobs.js'],
-  ['src/device/lldp.ts', 'lldp.js'],
-  ['src/device/config.ts', 'config.js'],
-  ['src/device/status.ts', 'status.js'],
-];
-
-/**
- * Run style bundle jobs.
- */
-async function bundleStyles() {
-  for (const [input, outFile] of styles) {
-    const instance = new Bundler(input, { outFile, ...options });
-    await instance.bundle();
-  }
-}
+const ARGS = process.argv.slice(2);
 
 /**
  * Run script bundle jobs.
  */
 async function bundleScripts() {
-  for (const [input, outFile] of scripts) {
-    const instance = new Bundler(input, { outFile, ...options });
-    await instance.bundle();
+  const entryPoints = {
+    netbox: 'src/index.ts',
+    jobs: 'src/jobs.ts',
+    lldp: 'src/device/lldp.ts',
+    config: 'src/device/config.ts',
+    status: 'src/device/status.ts',
+  };
+  try {
+    let result = await esbuild.build({
+      ...options,
+      entryPoints,
+      target: 'es2016',
+    });
+    if (result.errors.length === 0) {
+      for (const [targetName, sourceName] of Object.entries(entryPoints)) {
+        const source = sourceName.split('/')[1];
+        console.log(`✅ Bundled source file '${source}' to '${targetName}.js'`);
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+/**
+ * Run style bundle jobs.
+ */
+async function bundleStyles() {
+  try {
+    const entryPoints = {
+      'netbox-external': 'styles/_external.scss',
+      'netbox-light': 'styles/_light.scss',
+      'netbox-dark': 'styles/_dark.scss',
+      rack_elevations: 'styles/_rack_elevations.scss',
+      cable_trace: 'styles/_cable_trace.scss',
+    };
+    const pluginOptions = { outputStyle: 'compressed' };
+    // Allow cache disabling.
+    if (ARGS.includes('--no-cache')) {
+      pluginOptions.cache = false;
+    }
+    let result = await esbuild.build({
+      ...options,
+      entryPoints,
+      plugins: [sassPlugin(pluginOptions)],
+      loader: {
+        '.eot': 'file',
+        '.woff': 'file',
+        '.woff2': 'file',
+        '.svg': 'file',
+        '.ttf': 'file',
+      },
+    });
+    if (result.errors.length === 0) {
+      for (const [targetName, sourceName] of Object.entries(entryPoints)) {
+        const source = sourceName.split('/')[1];
+        console.log(`✅ Bundled source file '${source}' to '${targetName}.css'`);
+      }
+    }
+  } catch (err) {
+    console.error(err);
   }
 }
 
@@ -68,10 +86,10 @@ async function bundleScripts() {
  * Run all bundle jobs.
  */
 async function bundleAll() {
-  if (args.includes('--styles')) {
+  if (ARGS.includes('--styles')) {
     // Only run style jobs.
     return await bundleStyles();
-  } else if (args.includes('--scripts')) {
+  } else if (ARGS.includes('--scripts')) {
     // Only run script jobs.
     return await bundleScripts();
   }

netbox/project-static/dist/rack_elevation.css
@@ -1,2 +1,0 @@
-svg{--nbx-rack-bg:#e9ecef;--nbx-rack-border:#000;--nbx-rack-slot-bg:#e9ecef;--nbx-rack-slot-border:#adb5bd;--nbx-rack-slot-hover-bg:#ced4da;--nbx-rack-link-color:#0d6efd;--nbx-rack-unit-color:#6c757d}svg[data-netbox-color-mode=dark]{--nbx-rack-bg:#343a40;--nbx-rack-border:#6c757d;--nbx-rack-slot-bg:#343a40;--nbx-rack-slot-border:#495057;--nbx-rack-slot-hover-bg:#212529;--nbx-rack-link-color:#9ec5fe;--nbx-rack-unit-color:#6c757d}*{font-family:Inter,ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;font-size:.875rem}rect{box-sizing:border-box}text{text-anchor:middle;dominant-baseline:middle}svg .unit{margin:0;padding:5px 0;fill:var(--nbx-rack-unit-color)}svg .hidden{visibility:hidden}svg .rack{fill:none;stroke-width:2px;stroke:var(--nbx-rack-border);background-color:var(--nbx-rack-bg)}svg .slot{fill:var(--nbx-rack-slot-bg);stroke:var(--nbx-rack-slot-border)}svg .slot:hover{fill:var(--nbx-rack-slot-hover-bg)}svg .slot+.add-device{fill:var(--nbx-rack-link-color);opacity:0;pointer-events:none}svg .slot:hover+.add-device{opacity:1}svg .slot.reserved:hover[class]+.add-device{fill:#000}svg .slot.reserved:hover[class],svg .slot.reserved[class]{fill:url(#reserved)}svg .slot.occupied:hover[class],svg .slot.occupied[class]{fill:url(#occupied)}svg .slot.blocked:hover[class],svg .slot.blocked[class]{fill:url(#blocked)}svg .slot.blocked:hover+.add-device{opacity:0}
-/*# sourceMappingURL=/static/rack_elevation.css.map */

netbox/project-static/dist/cable_trace.css
@@ -1,2 +1,1 @@
-:root{--nbx-trace-color:#000;--nbx-trace-node-bg:#e9ecef;--nbx-trace-termination-bg:#f8f9fa;--nbx-trace-cable-shadow:#343a40;--nbx-trace-attachment:#ced4da}:root[data-netbox-color-mode=dark]{--nbx-trace-color:#fff;--nbx-trace-node-bg:#212529;--nbx-trace-termination-bg:#343a40;--nbx-trace-cable-shadow:#e9ecef;--nbx-trace-attachment:#6c757d}*{font-family:Inter,ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;font-size:.875rem}text{text-anchor:middle;dominant-baseline:middle}text:not([fill]){fill:var(--nbx-trace-color)}text.bold{font-weight:700}svg rect{fill:var(--nbx-trace-node-bg);stroke:var(--nbx-trace-node-bg);stroke-width:1}svg rect .termination{fill:var(--nbx-trace-termination-bg)}svg .connector text{text-anchor:start}svg line{stroke-width:5px}svg line.cable-shadow{stroke:var(--nbx-trace-cable-shadow);stroke-width:7px}svg line.attachment{stroke:var(--nbx-trace-attachment);stroke-dasharray:5px,5px}
-/*# sourceMappingURL=/static/cable_trace.css.map */
+:root{--nbx-trace-color: #000;--nbx-trace-node-bg: #e9ecef;--nbx-trace-termination-bg: #f8f9fa;--nbx-trace-cable-shadow: #343a40;--nbx-trace-attachment: #ced4da}:root[data-netbox-color-mode=dark]{--nbx-trace-color: #fff;--nbx-trace-node-bg: #212529;--nbx-trace-termination-bg: #343a40;--nbx-trace-cable-shadow: #e9ecef;--nbx-trace-attachment: #6c757d}*{font-family:Inter,ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";font-size:.875rem}text{text-anchor:middle;dominant-baseline:middle}text:not([fill]){fill:var(--nbx-trace-color)}text.bold{font-weight:700}svg rect{fill:var(--nbx-trace-node-bg);stroke:var(--nbx-trace-node-bg);stroke-width:1}svg rect .termination{fill:var(--nbx-trace-termination-bg)}svg .connector text{text-anchor:start}svg line{stroke-width:5px}svg line.cable-shadow{stroke:var(--nbx-trace-cable-shadow);stroke-width:7px}svg line.attachment{stroke:var(--nbx-trace-attachment);stroke-dasharray:5px,5px}

netbox/project-static/dist/cable_trace.css.map
@@ -1,1 +1,0 @@
-{"version":3,"sources":["cable-trace.scss","theme-base.scss"],"names":[],"mappings":"AAEA,MAEE,sBAAA,CACA,2BAAA,CACA,kCAAA,CACA,gCAAA,CACA,+BACA,mCAGE,sBAAA,CACA,2BAAA,CACA,kCAAA,CACA,gCAAA,CACA,+BAIJ,EACE,sMCuGuB,CDtGvB,kBAGF,KACE,kBAAA,CACA,yBAEA,iBAEE,4BAGF,UACE,gBAMF,SACE,6BAAA,CACA,+BAAA,CACA,eACA,sBACE,qCAKJ,oBACE,kBAEF,SACE,iBAEF,sBACE,oCAAA,CACA,iBAEF,oBACE,kCAAA,CACA","file":"cable_trace.css","sourceRoot":"../styles","sourcesContent":["// Cable Trace Styles.\n\n:root {\n  // Light Mode Variables.\n  --nbx-trace-color: #{$black};\n  --nbx-trace-node-bg: #{$gray-200};\n  --nbx-trace-termination-bg: #{$gray-100};\n  --nbx-trace-cable-shadow: #{$gray-800};\n  --nbx-trace-attachment: #{$gray-400};\n  &[data-netbox-color-mode='dark'] {\n    // Dark Mode Variables.\n    // Note: only the `theme-light.scss` file is in-scope for simplicity.\n    --nbx-trace-color: #{$white};\n    --nbx-trace-node-bg: #{$gray-900};\n    --nbx-trace-termination-bg: #{$gray-800};\n    --nbx-trace-cable-shadow: #{$gray-200};\n    --nbx-trace-attachment: #{$gray-600};\n  }\n}\n\n* {\n  font-family: $font-family-sans-serif;\n  font-size: $font-size-sm;\n}\n\ntext {\n  text-anchor: middle;\n  dominant-baseline: middle;\n\n  &:not([fill]) {\n    // Fill text with body color when a fill attribute is not set via element attributes.\n    fill: var(--nbx-trace-color);\n  }\n\n  &.bold {\n    font-weight: $font-weight-bold;\n  }\n}\n\nsvg {\n  /* Boxes */\n  rect {\n    fill: var(--nbx-trace-node-bg);\n    stroke: var(--nbx-trace-node-bg);\n    stroke-width: 1;\n    .termination {\n      fill: var(--nbx-trace-termination-bg);\n    }\n  }\n\n  /* Connectors */\n  .connector text {\n    text-anchor: start;\n  }\n  line {\n    stroke-width: 5px;\n  }\n  line.cable-shadow {\n    stroke: var(--nbx-trace-cable-shadow);\n    stroke-width: 7px;\n  }\n  line.attachment {\n    stroke: var(--nbx-trace-attachment);\n    stroke-dasharray: 5px, 5px;\n  }\n}\n","// Base NetBox Theme Overrides and Settings - color mode agnostic.\n\n@import 'bootstrap/scss/functions';\n\n$card-cap-bg: 'unset';\n\n$border-radius-md: 0.375rem;\n$border-radius-lg: 0.5rem;\n$border-radius-xl: 0.75rem;\n$border-radius-2xl: 1.5rem;\n\n$border-radius: $border-radius-lg;\n\n$border-radius-sm: $border-radius;\n$border-radius-lg: $border-radius-xl;\n\n$badge-border-radius: $border-radius-md;\n$progress-border-radius: $border-radius-md;\n\n$font-weight-lighter: 200;\n$font-weight-medium: 600;\n$font-weight-bolder: 800;\n\n$font-size-xs: 0.75rem;\n\n$line-height-base: 1.5;\n$line-height-xs: 1;\n$line-height-sm: 1.25;\n$line-height-lg: 1.75;\n\n$darker: #1b1f22;\n$darkest: #171b1d;\n\n@import 'bootstrap/scss/variables';\n\n// Make color palette colors available as theme colors.\n// For example, you could use `.bg-red-100`, if needed.\n$theme-color-addons: (\n  'darker': $darker,\n  'darkest': $darkest,\n  'gray': $gray-400,\n  'gray-100': $gray-100,\n  'gray-200': $gray-200,\n  'gray-300': $gray-300,\n  'gray-400': $gray-400,\n  'gray-500': $gray-500,\n  'gray-600': $gray-600,\n  'gray-700': $gray-700,\n  'gray-800': $gray-800,\n  'gray-900': $gray-900,\n  'red-100': $red-100,\n  'red-200': $red-200,\n  'red-300': $red-300,\n  'red-400': $red-400,\n  'red-500': $red-500,\n  'red-600': $red-600,\n  'red-700': $red-700,\n  'red-800': $red-800,\n  'red-900': $red-900,\n  'yellow-100': $yellow-100,\n  'yellow-200': $yellow-200,\n  'yellow-300': $yellow-300,\n  'yellow-400': $yellow-400,\n  'yellow-500': $yellow-500,\n  'yellow-600': $yellow-600,\n  'yellow-700': $yellow-700,\n  'yellow-800': $yellow-800,\n  'yellow-900': $yellow-900,\n  'green-100': $green-100,\n  'green-200': $green-200,\n  'green-300': $green-300,\n  'green-400': $green-400,\n  'green-500': $green-500,\n  'green-600': $green-600,\n  'green-700': $green-700,\n  'green-800': $green-800,\n  'green-900': $green-900,\n  'blue-100': $blue-100,\n  'blue-200': $blue-200,\n  'blue-300': $blue-300,\n  'blue-400': $blue-400,\n  'blue-500': $blue-500,\n  'blue-600': $blue-600,\n  'blue-700': $blue-700,\n  'blue-800': $blue-800,\n  'blue-900': $blue-900,\n  'cyan-100': $cyan-100,\n  'cyan-200': $cyan-200,\n  'cyan-300': $cyan-300,\n  'cyan-400': $cyan-400,\n  'cyan-500': $cyan-500,\n  'cyan-600': $cyan-600,\n  'cyan-700': $cyan-700,\n  'cyan-800': $cyan-800,\n  'cyan-900': $cyan-900,\n  'indigo-100': $indigo-100,\n  'indigo-200': $indigo-200,\n  'indigo-300': $indigo-300,\n  'indigo-400': $indigo-400,\n  'indigo-500': $indigo-500,\n  'indigo-600': $indigo-600,\n  'indigo-700': $indigo-700,\n  'indigo-800': $indigo-800,\n  'indigo-900': $indigo-900,\n  'purple-100': $purple-100,\n  'purple-200': $purple-200,\n  'purple-300': $purple-300,\n  'purple-400': $purple-400,\n  'purple-500': $purple-500,\n  'purple-600': $purple-600,\n  'purple-700': $purple-700,\n  'purple-800': $purple-800,\n  'purple-900': $purple-900,\n  'pink-100': $pink-100,\n  'pink-200': $pink-200,\n  'pink-300': $pink-300,\n  'pink-400': $pink-400,\n  'pink-500': $pink-500,\n  'pink-600': $pink-600,\n  'pink-700': $pink-700,\n  'pink-800': $pink-800,\n  'pink-900': $pink-900,\n);\n\n$font-family-sans-serif: Inter, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont,\n  'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji',\n  'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';\n$font-family-monospace: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono',\n  'Courier New', monospace;\n\n$accordion-padding-y: 0.8125rem;\n$accordion-padding-x: 0.8125rem;\n\n$sidebar-width: 280px;\n$sidebar-bottom-height: 4rem;\n"]}

commit c398e00833038a8c2837ca3d1200da12f1208840

Fix incorrect rack elevation file name regression from dcfa5d1

netbox/project-static/bundle.js
@@ -51,7 +51,7 @@ async function bundleStyles() {
       'netbox-external': 'styles/_external.scss',
       'netbox-light': 'styles/_light.scss',
       'netbox-dark': 'styles/_dark.scss',
-      rack_elevations: 'styles/_rack_elevations.scss',
+      rack_elevation: 'styles/_rack_elevation.scss',
       cable_trace: 'styles/_cable_trace.scss',
     };
     const pluginOptions = { outputStyle: 'compressed' };

netbox/project-static/styles/_rack_elevation.scss
@@ -1,0 +1,2 @@
+@import './theme-light.scss';
+@import './rack-elevation.scss';

netbox/project-static/styles/_rack_elevations.scss
@@ -1,2 +1,0 @@
-@import './theme-light.scss';
-@import './rack-elevations.scss';